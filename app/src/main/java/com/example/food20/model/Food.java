9da0ad5f76d61c8560ccec77fdb632d33a641daa
Anderson Almeida <110561674+Ninguchot@users.noreply.github.com>
2023-09-25 22:36:01 -0300

    Foi feito um model para os foods, adicionado um set e get

---
+++ app/src/main/java/com/example/food20/model/Food.java
@@ -0,0 +1,41 @@
+package com.example.food20.model;
+
+public class Food {
+    int imgFood;
+    String foodName;
+    String foodDescription;
+    String price;
+
+
+    public int getImgFood() {
+        return imgFood;
+    }
+
+    public void setImgFood(int imgFood) {
+        this.imgFood = imgFood;
+    }
+
+    public String getFoodName() {
+        return foodName;
+    }
+
+    public void setFoodName(String foodName) {
+        this.foodName = foodName;
+    }
+
+    public String getFoodDescription() {
+        return foodDescription;
+    }
+
+    public void setFoodDescription(String foodDescription) {
+        this.foodDescription = foodDescription;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+}
